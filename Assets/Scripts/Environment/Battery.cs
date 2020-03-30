84086c22d9fc7337ce20371f69e786a3a418bc95
TheMiracleMango <eduong@nevada.unr.edu>
2020-03-29 19:19:38 -0700

    Adjusted Battery Life

--- Assets/Scripts/Environment/Battery.cs
+++ Assets/Scripts/Environment/Battery.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class Battery : MonoBehaviour
 {
-    public float batteryLife = 10;
+    public float batteryLife = 1;
     public float batteryDrainRateRelativeToSpeed;
     public float batteryDrainRateServingUsers;
     public float batteryDrainRateConstant;
 
     private BryansPhysics physics;
     private NetworkRouter router;
+    private bool running;
 
     // Start is called before the first frame update
     void Start()
     {
         physics = transform.GetComponent<BryansPhysics>();
         router = transform.GetComponent<NetworkRouter>();
+        running = true;
     }
 
     // Update is called once per frame
@@ -24,7 +26,14 @@ public class Battery : MonoBehaviour
     {
         if (batteryLife <= 0)
         {
-            router.connectedRouters.Clear();
+            if (running)
+            {
+                router.cm.allRouters.Remove(router.GetID());
+                router.connectedRouters.Clear();
+                router.transform.parent.GetComponent<Node>().UAV = null;
+                router.transform.parent = router.transform.parent.transform.parent;
+                running = false;
+            }
             physics.desiredSpeed = 0;
             physics.desiredHeading = 0;
             physics.desiredVerticalHeading = 0;
