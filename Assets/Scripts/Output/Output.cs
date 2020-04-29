7abefe969c0177fee9dc404b087c2de2a7ff99df
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-04-28 23:58:27 -0700

    Release running UI display.

--- Assets/Scripts/Output/Output.cs
+++ Assets/Scripts/Output/Output.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
 using System;
 using System.IO;
+using UnityEngine.UI;
 
 //A class which is used to add the current date and time to the output file so it outputs to a different file each time the simulation runs.
 public static class MyExtensions
@@ -38,6 +39,9 @@ public class Output : MonoBehaviour
 
     //Variable to ensure that output file is not updated once all of the UAV's have died.
     private bool stopWriting = false;
+
+    //The UI to write text to.
+    public Text UIText;
 
     //Function to generate the header for the parameter output file.
     private string generateHeader()
@@ -98,7 +102,8 @@ public class Output : MonoBehaviour
     }
 
     //Function used to generate the data which is written to the data output file every second.
-    private string CreateStringOfData()
+    //Param delimiter is the delimiter seperating the data.
+    private string CreateStringOfData(string delimiter)
     {
         double timeStamp = (System.DateTime.Now - startTime).TotalSeconds;
         int totalConnectedUsers = NetworkManager.inst.totalConnectedUsers;
@@ -148,7 +153,62 @@ public class Output : MonoBehaviour
         {
             stopWriting = true;
         }
-        return timeStamp.ToString() + "," + totalConnectedUsers + "," + totalDisconnectedusers + "," + averageUserDisconnectTime + "," + priority1UserAverageConnectionTime + "," + priority2UserAverageConnectionTime + "," + priority3UserAverageConnectionTime + "," + totalActiveUAVs + "," + averageUAVTravelDistance;
+        return timeStamp.ToString() + delimiter + totalConnectedUsers + delimiter + totalDisconnectedusers + delimiter + averageUserDisconnectTime + delimiter + priority1UserAverageConnectionTime + delimiter + priority2UserAverageConnectionTime + delimiter + priority3UserAverageConnectionTime + delimiter + totalActiveUAVs + delimiter + averageUAVTravelDistance;
+    }
+
+    //Function used to generate the data which is written to the data output file every second.
+    //Param delimiter is the delimiter seperating the data.
+    private string CreateStringOfDataWithLabels(string delimiter)
+    {
+        double timeStamp = (System.DateTime.Now - startTime).TotalSeconds;
+        int totalConnectedUsers = NetworkManager.inst.totalConnectedUsers;
+        int totalDisconnectedusers = EntityManager.inst.users.Count - totalConnectedUsers;
+        int totalUsers = EntityManager.inst.users.Count;
+        int totalActiveUAVs = NetworkManager.inst.routers.Count - 1;
+
+        int totalUAVs = EntityManager.inst.uavs.Count;
+        float averageUserDisconnectTime = 0;
+        float priority1UserAverageConnectionTime = 0;
+        int priority1UserCount = 0;
+        float priority2UserAverageConnectionTime = 0;
+        int priority2UserCount = 0;
+        float priority3UserAverageConnectionTime = 0;
+        int priority3UserCount = 0;
+        float averageUAVTravelDistance = 0;
+
+        foreach (UserEntity user in EntityManager.inst.users)
+        {
+            averageUserDisconnectTime += user.device.timeDisconnected / totalUsers;
+
+            if (user.device.priority == 1)
+            {
+                priority1UserAverageConnectionTime += user.device.timeConnected;
+                ++priority1UserCount;
+            }
+            else if (user.device.priority == 2)
+            {
+                priority2UserAverageConnectionTime += user.device.timeConnected;
+                ++priority2UserCount;
+            }
+            else
+            {
+                priority3UserAverageConnectionTime += user.device.timeConnected;
+                ++priority3UserCount;
+            }
+        }
+        priority1UserAverageConnectionTime /= priority1UserCount;
+        priority2UserAverageConnectionTime /= priority2UserCount;
+        priority3UserAverageConnectionTime /= priority3UserCount;
+
+        foreach (UAVEntity uav in EntityManager.inst.uavs)
+        {
+            averageUAVTravelDistance += uav.physics.distanceTraveled / totalUAVs;
+        }
+        if (totalActiveUAVs == 0)
+        {
+            stopWriting = true;
+        }
+        return "Simulation Time(s): " + ((int) timeStamp) + delimiter + "Connected Devices: " + totalConnectedUsers + delimiter + "Disconnected Devices: " + totalDisconnectedusers + delimiter + "Average Device Disconnect Time(s): " + Mathf.Round(averageUserDisconnectTime * 100f)/100f + delimiter + "Average Priority 1 Connection Time(s): " + Mathf.Round(priority1UserAverageConnectionTime*100f)/100f + delimiter + "Average Priority 2 Connection Time(s): " + Mathf.Round(priority2UserAverageConnectionTime*100f)/100f + delimiter + "Average Priority 3 Connection Time(s): " + Mathf.Round(priority3UserAverageConnectionTime*100f)/100f + delimiter + "Active UAVs: " + totalActiveUAVs + delimiter + "Average UAV Travel Distance(m): " + Mathf.Round(averageUAVTravelDistance*100f)/100f;
     }
 
     //Function used to generate the data which is written parameter output to the file.
@@ -169,7 +229,7 @@ public class Output : MonoBehaviour
         if (timeRemaining < 0 && NetworkManager.inst.routers.Count > 0 && !stopWriting)
         {
             timeRemaining = updateFrequency;
-            WriteOutputToFile(CreateStringOfData());
+            WriteOutputToFile(CreateStringOfData(","));
         }
         timeRemaining -= Time.deltaTime;
         if (timeRemaining < 0 && !parameterWritten)
@@ -178,5 +238,6 @@ public class Output : MonoBehaviour
             parameterWritten = true;
         }
         parameterTimeRemaining -= Time.deltaTime;
+        UIText.text = CreateStringOfDataWithLabels("\n");
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# CPE400 Group Project - Reconfigurable UAV network
+# Re-configurable Mesh Network with Limited UAVs Simulation
 
-## Unity Version
-This project is compatible with Unity Version LTS Release 2018.4.19f1 Released: 13 March 2020 and above. 
+Unmanned Aerial Vehicle (UAV) networks can provide coverage to users in areas where infrastructure is not available. Each UAV forms a circular area in which they serve the users. Limited resources can introduce constraints in the number of UAVs available to serve the coverage area. Along with efficient deployment and dispatch based on the assumption that the region has already been explored, this paper presents an approach to handle the problem of limitations in available UAVs when forming a dynamic mesh network. The approach uses a greedy algorithm that runs dynamically to re-position to provide Internet services to the maximum number of users for as long as possible.
+
+## Getting Started for Users
+To view the running simulation visit this link: 
 
 ## Getting Started for Developers
 1) Download this repository using [Github Desktop](https://desktop.github.com/)
-2) Open [Unity Hub](https://unity3d.com/get-unity/download) and make sure Unity Version 2018.4.19f1 is installed
+2) Open [Unity Hub](https://unity3d.com/get-unity/download) and make sure Unity Version 2018.4.19f1 or greater is installed
 3) Open this project through Unity Hub by locating the downloaded repository from Github on your machine
 
-## Getting Started for Users
+## Metadata
+CPE400 Computer Communication Networks
+Instructor:Amar Nath Patra
+Developed By: Eric Duong, Yash Shah, Bryan Dedeurwaerder
+
+
